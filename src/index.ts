91cf1b9daa479786ff72368827a3a3133bd899ed
Juan <abrecan.eyndrill@gmail.com>
2020-01-02 00:08:56 -0500

    testing

--- src/index.ts
+++ src/index.ts
@@ -14,7 +14,7 @@ mongoose.connection.on('error', console.error);
 
 const app = new Koa();
 const router = new KoaRouter({
-	prefix: process.env.NODE_ENV === 'local' ? 'api' : '/.netlify/functions/server'
+	prefix: 'api'
 });
 const controller = new PlayerController();
 
