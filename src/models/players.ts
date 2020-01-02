58bbb477c34ebdcb48df4289ab1fb69b89418201
Juan <abrecan.eyndrill@gmail.com>
2020-01-02 00:54:35 -0500

    hail mary

--- src/models/players.ts
+++
@@ -1,6 +0,0 @@
-import { Document, Schema, Model, model } from 'mongoose';
-import { PlayerSchema, IPlayerModel } from '../schemas/player';
-
-export const Players: Model<IPlayerModel> = model<IPlayerModel>('players', PlayerSchema);
-
-export default Players;
